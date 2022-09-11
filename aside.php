d98ac5ffea70901cc94331f86b74aa1d64b60618
Eric Venturino <eric.venturino@orange.fr>
2022-09-11 20:59:34 +0200

    Premier envoi

---
+++ aside.php
@@ -0,0 +1,15 @@
+<aside>
+            <div class="module1">
+                <label for="email">e-mail</label>
+                <input type="email" placeholder="email" id="email" name="email">
+                <label for="motdepasse">mot de passe</label>
+                <input type="password" name="mdp" id="mdp" placeholder="mot de passe">
+                <div>
+                    <button type="submit">Connexion</button>
+                </div>
+            </div>
+            <div class="minion">
+                <img class="imgminion" src="./minion.gif" alt="Les minions">
+            </div>
+        </aside>
+    </main>
---
+++ index.php
@@ -0,0 +1,11 @@
+    <!-- Lancer un serveur depuis la console php: php -S localhost:8000 -->
+    <?php
+    $title= "Page Afpa";
+    require('./header.php');
+
+    require('./section.php');
+
+    require('./aside.php');
+
+    require('./footer.php');
+    ?>
